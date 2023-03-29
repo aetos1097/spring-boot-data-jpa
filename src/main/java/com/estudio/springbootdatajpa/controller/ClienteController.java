5c72d1d208e31372d2ed624710f3c2c33c5913b9
Juan Pablo <aetosyaoista97@gmail.com>
2023-03-29 16:15:36 -0500

    Se agregaron las validaciones desde la entidad, asi mismo se actualizo el formulario para que se muestre los errores y se personalizo los errores con el archivo messages.properties, sin olvidar muy importante introducir la dependencia(en el pom.xml): satarter-validate para que esto funcione.

--- src/main/java/com/estudio/springbootdatajpa/controller/ClienteController.java
+++ src/main/java/com/estudio/springbootdatajpa/controller/ClienteController.java
@@ -2,9 +2,11 @@ package com.estudio.springbootdatajpa.controller;
 
 import com.estudio.springbootdatajpa.models.dao.IClienteDao;
 import com.estudio.springbootdatajpa.models.entity.Cliente;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,8 +35,17 @@ public class ClienteController {
         return "form";
     }//guardamos los eleementos
     @PostMapping("/form")
-    public String guardar(Cliente cliente){
-        clienteDao.save(cliente);
+    public String guardar(@Valid Cliente cliente, BindingResult result,Model model){//colocamos la anotacion Valid en el argumento porque sera lo que se envia y debe estar validado
+        //Siempre van juntos El objeto con @Valid y el BindingResult y de resto lo demas
+        //El BindinfResult es una interfaz que captura los errores en las validaciones
+        //Ahora validamos con un if si hay algun error en los campos del formulario
+        if(result.hasErrors()){
+            //si el objeto se llama igual que en el formulario con metodo get entonces se pasa automatico
+            //si no se debe poner en los argumentos :(@Valid @ModelAttribute Cliente cliente, BindingResult result,Model model)
+            model.addAttribute("titulo","Formulario Cliente");
+            return "form";//si hay errores nos devolvemos al formulario con ruta /form
+        }
+        clienteDao.save( cliente);
         return  "redirect:listar";
     }
 }
--- src/main/java/com/estudio/springbootdatajpa/models/entity/Cliente.java
+++ src/main/java/com/estudio/springbootdatajpa/models/entity/Cliente.java
@@ -1,6 +1,10 @@
 package com.estudio.springbootdatajpa.models.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -12,18 +16,30 @@ public class Cliente implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)//indica que sera autoincrementable el id
     private Long id;//este atributo es la llave
     //se puede colocar la anotacion @Column para cambiar el nombre,tamanio,nullable(cutomizar) a una variable al de la tabla en la db
+    @NotEmpty//esta anotacion valida que no este vacion peor SOLO con Strings
+    //@Size(min=1,max=20) permite darle un tamanio al campo de caracteres
     private String nombre;
+
+    @NotEmpty
     private String apellido;
+
+    @NotEmpty
+    @Email//Validacion por correo
     private String email;
+
+    @NotNull//Todos los demas tipos no STRING se pueden validar con esta anotacion
     @Column(name = "create_at")
     @Temporal(TemporalType.DATE)//solamente para fechas, indica el formato en que se gurdara esta fecha en la tabla
+    @DateTimeFormat(pattern = "yyyy-MM-dd") //asignamos el aptron de fecha como lo deseemos
     private Date createAt;
+    /*
+    Ya no sera necesario este metodo porque se creara un nuevo campo en el html
     //@anotacion en la entidad que creara la fecha justo antes de insertar el registro en la base de datos
     @PrePersist
     public void prePersisit(){
         //metodo para registrar antes que se guarde a la base de datos
         createAt = new Date();
-    }
+    }*/
     private static final long serialVersionUID = 1L;//esta línea de código sirve para mantener la compatibilidad en la serialización de objetos en Java al proporcionar un identificador único a cada clase serializable.
 
     public Long getId() {
