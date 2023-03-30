a3993ca274e67a539b02bfc1cc71f26500e2f94f
Juan Pablo <aetosyaoista97@gmail.com>
2023-03-30 00:02:44 -0500

    solo se puso un comentario explicativo

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,5 @@
 /*populate tables */
+-- Archivo importante para introducir datos al inicio, el archivo se debe llamar igual que este ademas no se introduce el campo id
+-- porque este es autoincrementable en la entidad.
 INSERT INTO clientes( nombre, apellido, email, create_at) VALUES ('Juan', 'Medina','aetos@gamil.com','2018-08-21');
 INSERT INTO clientes( nombre, apellido, email, create_at) VALUES ('Andre', 'Pena','aetos23@gamil.com','2018-08-22');
